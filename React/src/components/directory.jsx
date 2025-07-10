11eb757e64bebfa9e5b26162d6dc3e6285153573
Garrett Willoughby <garrettwilloughby@gmail.com>
2025-07-10 13:59:22 -0400

    changes so far

--- React/src/components/directory.jsx
+++ React/src/components/directory.jsx
@@ -1,9 +1,6 @@
 import {React, useState, useEffect} from "react";
 import Table from "../utils/datatable";
 
-//delete later
-import fakeEmployees from "../sample_employees";
-
 function Directory(){
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -13,19 +10,18 @@ function Directory(){
 
         
         const fetchData = async () => {
-            setData(fakeEmployees);
-        //   try {
-        //     const response = await fetch('/api/data');
-        //     if (!response.ok) {
-        //       throw new Error(`HTTP error! status: ${response.status}`);
-        //     }
-        //     const result = await response.json();
-        //     setData(result);
-        //   } catch (err) {
-        //     setError(err);
-        //   } finally {
-        //     setLoading(false);
-        //   }
+            try {
+                const response = await fetch('http://localhost:3000/api/0');
+                if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
         };
         fetchData();
       }, []);
