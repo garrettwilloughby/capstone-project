8ceb733e53ef62bd3c6907ec79029a07161ea842
Alice Smith <alice@webage.com>
2025-07-11 14:11:32 -0400

    salary pattern updates

--- ds/generate_data.py
+++ ds/generate_data.py
@@ -1,18 +1,6 @@
 import pandas as pd
 import random
 
-# names = [
-# "Thomas Parsons"
-# "Brandon Dickson"
-# "Hasan Barlow"
-# "Zack Rodriguez"
-# "Bertha Barker"
-# "Yasin Small"
-# "Oisin Nelson"
-# "Talia Dominguez"
-# "Isobel Jefferson"
-# "Riya Pratt"
-# ]
 
 job_roles = [
     "Software Engineer",
@@ -30,23 +18,46 @@ locations = [
     "Shanghai"
 ]
 
-def salary (job, loc):
-    base = 80000 + 3000 * job_roles.index(job)
-    loc_bonus = 5000 * locations.index(loc)
-    noise = random.randint(-3000, 3000)
-    return base + loc_bonus + noise
+role_base_salary = {
+    "Software Engineer": 100000,
+    "Data Engineer": 95000,
+    "Data Anaylst": 85000,
+    "UX Designer": 80000,
+    "Devops Engineer": 97000
+}
+
+location_bonus = {
+    "London": 10000,
+    "New York": 15000,
+    "San Francisco": 20000,
+    "Cairo": 0,
+    "Shanghai": 5000
+}
+
+
+
+
 
 rows = []
+
 for _ in range(1000):
     job = random.choice(job_roles)
     loc = random.choice(locations)
-    rows.append({
-        "job_roles": job,
+
+    base = role_base_salary[job]
+    loc_bonus = location_bonus[loc]
+    noise = random.randint(-3000, 3000)
+
+    salary = base + loc_bonus + noise
+    row = {
+         "job_roles": job,
         "location": loc,
-        "salary": salary(job, loc)
-    })
+        "salary": salary
+    }
+
+    rows.append(row)
 
 df = pd.DataFrame(rows)
 df.to_csv("employee_data.csv", index=False)
-print("1000 employee recors written to employee_data.csv")
+print("Wrote rows", len(df))
     
